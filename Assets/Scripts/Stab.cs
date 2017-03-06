ca3d593b58b73e52edd6a19da76b2bf7d5cae75a
Mykado72 <mickael.lehay@gmail.com>
2017-03-06 12:29:19 +0100

    Stab

---
+++ Assets/Scripts/Stab.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stab : MonoBehaviour {
+
+
+    public float stability = 0.1f;
+    public float speed = 2.0f;
+    public Rigidbody rgChassi;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 predictedUp = Quaternion.AngleAxis(
+            rgChassi.angularVelocity.magnitude * Mathf.Rad2Deg * stability / speed,
+            rgChassi.angularVelocity
+        ) * transform.up;
+        Vector3 torqueVector = Vector3.Cross(predictedUp, Vector3.up);
+        rgChassi.AddTorque(torqueVector * speed * speed);
+    }
+
+}

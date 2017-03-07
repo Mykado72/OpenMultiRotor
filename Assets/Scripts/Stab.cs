e96c459b902b4702dc4c114dc25ca9712f3fa9e9
Mykado72 <mickael.lehay@gmail.com>
2017-03-07 18:52:47 +0100

    Angles

--- Assets/Scripts/Stab.cs
+++ Assets/Scripts/Stab.cs
@@ -8,15 +8,38 @@ public class Stab : MonoBehaviour {
     public float stability = 0.1f;
     public float speed = 2.0f;
     public Rigidbody rgChassi;
+    public Vector3 vpredictedUp;
+    public float anglePitch;
+    public float angleRoll;
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 predictedUp = Quaternion.AngleAxis(
-            rgChassi.angularVelocity.magnitude * Mathf.Rad2Deg * stability / speed,
-            rgChassi.angularVelocity
-        ) * transform.up;
-        Vector3 torqueVector = Vector3.Cross(predictedUp, Vector3.up);
-        rgChassi.AddTorque(torqueVector * speed * speed);
+        Quaternion qpredictedUp = Quaternion.AngleAxis(rgChassi.angularVelocity.magnitude * Mathf.Rad2Deg, rgChassi.angularVelocity); 
+        vpredictedUp = qpredictedUp.eulerAngles;
+        // Debug.Log(vpredictedUp);
+        // Vector3 torqueVector = Vector3.Cross(predictedUp, Vector3.up);
+        // rgChassi.AddTorque(torqueVector * speed * speed);
+        anglePitch = PosNegAngleY(Vector3.up, transform.forward);  //     Vector3.Angle(transform.forward, Vector3.up);
+        angleRoll = PosNegAngleX(Vector3.up, transform.right);  // Vector3.Angle(transform.right, Vector3.up);
+        //transform.up //+y axis
+        //transform.forward //+z axis
+        // transform.right //+x axis
+    }
+
+    public float PosNegAngleY(Vector3 a1, Vector3 a2)
+    {
+        float angle = Vector3.Angle(a1, a2);
+        Vector3 cross = Vector3.Cross(a1, a2);
+        if (cross.y < 0) angle = -angle;
+        return angle;
+    }
+
+    public float PosNegAngleX(Vector3 a1, Vector3 a2)
+    {
+        float angle = Vector3.Angle(a1, a2);
+        Vector3 cross = Vector3.Cross(a1, a2);
+        if (cross.x < 0) angle = -angle;
+        return angle;
     }
 
 }

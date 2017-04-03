9001398f27d8aafe0cc81ace2cfa19ddfbb54ec0
Mykado72 <mickael.lehay@gmail.com>
2017-04-03 19:59:17 +0200

    Les bras peuvent maintenant casser

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour {
 
+    public GameObject goToBreakToo;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,14 +14,21 @@ public class Damage : MonoBehaviour {
     {
         Debug.Log("A joint has just been broken!, force: " + breakForce);
         transform.GetComponent<Rigidbody>().useGravity = true;
+        if (goToBreakToo!=null)
+        {
+            Debug.Log(goToBreakToo.name+ "is break");
+            Destroy(goToBreakToo.GetComponent<FixedJoint>());
+            goToBreakToo.GetComponent<Rigidbody>().useGravity = true;
+        }
+        
     }
 
-    private void OnCollisionEnter(Collision collision)
+  /*  private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision à la vitesse :" + collision.relativeVelocity.magnitude+ " de " + this.name + " avec " + collision.gameObject.name);
+        Debug.Log("Collision à la vitesse :" + collision.relativeVelocity.magnitude+ " de " + this.name + " avec " + collision.gameObject.name);    
 
         // if (collision.relativeVelocity.magnitude > 10)
             // this.GetComponentInParent<Rigidbody>().useGravity = true;
     }
-
+    */
 }

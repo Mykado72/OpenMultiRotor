01670d2f4044f8e5bc4163c036682b5daabbe98e
Mykado72 <mickael.lehay@gmail.com>
2017-04-03 12:16:17 +0200

    Ajout Colliders au niveau des bras

---
+++ Assets/Scripts/DoSomethingInCoroutine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoSomethingInCoroutine : MonoBehaviour {
+
+    public int timerCoroutine;
+    void Start()
+    {
+        StartCoroutine(CheckSomething());
+    }
+
+    IEnumerator CheckSomething()
+    {
+        var wait = new WaitForSeconds(0.2f);
+        while (true)
+        {
+            yield return wait;
+            //NowRotating = (rot != this.transform.rotation);
+            timerCoroutine++;
+            //where Tolerance is small... like 0.01.
+            //due to float error it might be better to use this... your choice
+        }
+    }
+}

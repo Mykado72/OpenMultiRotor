27c0abc2a08b727ba78da5043f2b04c0cb3935cc
Mykado72 <mickael.lehay@gmail.com>
2017-04-26 18:33:06 +0200

    Un peu mieux

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -164,10 +164,11 @@ public class Controls : MonoBehaviour {
             targetDir.y = 0;
             Quaternion rotation = Quaternion.LookRotation(targetDir);
             consignVector.y = Mathf.Clamp(AIControlPID.yawPID.Update(rgChassi.position.x+ RelativeWaypointPosition.x, rgChassi.position.x, stabspeed * delta),-1f,+1f);
-            consignVector.z = Mathf.Clamp(AIControlPID.rollPID.Update(rgChassi.position.x + RelativeWaypointPosition.x, rgChassi.position.x, stabspeed * delta)+ consignVector.y*1.65f, -3f, +3f);
+            // consignVector.z = Mathf.Clamp(AIControlPID.rollPID.Update(rgChassi.position.x + RelativeWaypointPosition.x, rgChassi.position.x, stabspeed * delta)+ consignVector.y*0.5f, -3f, +3f);
+            consignVector.z = consignVector.y * 1.25f;
             consignVector.x = Mathf.Clamp(AIControlPID.pitchPID.Update(rgChassi.position.z + RelativeWaypointPosition.z, rgChassi.position.z, stabspeed * delta), -1f, +1f);
             if (rgChassi.position.y < targetWaypoint.position.y)
-                throttlecomp = Mathf.Abs(rgChassi.rotation.eulerAngles.z) * 0.00001f + Mathf.Abs(rgChassi.rotation.eulerAngles.x) * 0.7f;
+                throttlecomp = Mathf.Abs(rgChassi.rotation.eulerAngles.z) * 0.0002f + Mathf.Abs(rgChassi.rotation.eulerAngles.x) * 0.2f;
             else
                 throttlecomp = 0;
             throttle = Mathf.Clamp(AIControlPID.throttlePID.Update(targetWaypoint.position.y, rgChassi.position.y, stabspeed * delta)+throttlecomp, -1f, +1f);
--- Assets/Standard Assets/Cameras/Scripts/AutoCam.cs
+++ Assets/Standard Assets/Cameras/Scripts/AutoCam.cs
@@ -14,9 +14,12 @@ namespace UnityStandardAssets.Cameras
         [SerializeField] private float m_RollSpeed = 0.2f;// How fast the rig will roll (around Z axis) to match target's roll.
         [SerializeField] private bool m_FollowVelocity = false;// Whether the rig will rotate in the direction of the target's velocity.
         [SerializeField] private bool m_FollowTilt = true; // Whether the rig will tilt (around X axis) with the target.
+        [SerializeField] private float m_SpinTurnLimit = 90;// The threshold beyond which the camera stops following the target's rotation. (used in situations where a car spins out, for example)
         [SerializeField] private float m_TargetVelocityLowerLimit = 4f;// the minimum velocity above which the camera turns towards the object's velocity. Below this we use the object's forward direction.
+        [SerializeField] private float m_SmoothTurnTime = 0.2f; // the smoothing for the camera's rotation
 
         private float m_LastFlatAngle; // The relative angle of the target and the rig from the previous frame.
+        private float m_CurrentTurnAmount; // How much to turn the camera
         private float m_TurnSpeedVelocityChange; // The change in the turn speed velocity
         private Vector3 m_RollUp = Vector3.up;// The roll of the camera around the z axis ( generally this will always just be up )
 
@@ -47,7 +50,8 @@ namespace UnityStandardAssets.Cameras
                 else
                 {
                     targetUp = Vector3.up;
-                }                
+                }
+                m_CurrentTurnAmount = Mathf.SmoothDamp(m_CurrentTurnAmount, 1, ref m_TurnSpeedVelocityChange, m_SmoothTurnTime);
             }
             else
             {
@@ -57,6 +61,26 @@ namespace UnityStandardAssets.Cameras
                 // eg when a car has been knocked into a spin. The camera will resume following the rotation
                 // of the target when the target's angular velocity slows below the threshold.
                 var currentFlatAngle = Mathf.Atan2(targetForward.x, targetForward.z)*Mathf.Rad2Deg;
+                if (m_SpinTurnLimit > 0)
+                {
+                    var targetSpinSpeed = Mathf.Abs(Mathf.DeltaAngle(m_LastFlatAngle, currentFlatAngle))/deltaTime;
+                    var desiredTurnAmount = Mathf.InverseLerp(m_SpinTurnLimit, m_SpinTurnLimit*0.75f, targetSpinSpeed);
+                    var turnReactSpeed = (m_CurrentTurnAmount > desiredTurnAmount ? .1f : 1f);
+                    if (Application.isPlaying)
+                    {
+                        m_CurrentTurnAmount = Mathf.SmoothDamp(m_CurrentTurnAmount, desiredTurnAmount,
+                                                             ref m_TurnSpeedVelocityChange, turnReactSpeed);
+                    }
+                    else
+                    {
+                        // for editor mode, smoothdamp won't work because it uses deltaTime internally
+                        m_CurrentTurnAmount = desiredTurnAmount;
+                    }
+                }
+                else
+                {
+                    m_CurrentTurnAmount = 1;
+                }
                 m_LastFlatAngle = currentFlatAngle;
             }
 
@@ -77,7 +101,7 @@ namespace UnityStandardAssets.Cameras
 
             // and aligning with the target object's up direction (i.e. its 'roll')
             m_RollUp = m_RollSpeed > 0 ? Vector3.Slerp(m_RollUp, targetUp, m_RollSpeed*deltaTime) : Vector3.up;
-            transform.rotation = Quaternion.Lerp(transform.rotation, rollRotation, m_TurnSpeed*deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rollRotation, m_TurnSpeed*m_CurrentTurnAmount*deltaTime);
         }
     }
 }
